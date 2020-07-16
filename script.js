d864d20bae8aa1fd28541a675900a8b1ee4ef411
Ian A <iastesana@gmail.com>
2020-07-15 19:30:08 -0700

    Finished the last piece, and it is now working as intended.

--- script.js
+++ script.js
@@ -1,69 +1,63 @@
 // Assignment Code
 // ID of the button
 var generateBtn = document.querySelector("#generate");
-var upperArray="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var lowerArray="abcdefghijklmnopqrstuvwxyz"
-var numberArray="123456789"
-var symbolArray="-_!@#$%^&*()"
-var getUserChoice=[]
-var passwordLength=0
+var upperArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerArray = "abcdefghijklmnopqrstuvwxyz"
+var numberArray = "123456789"
+var symbolArray = "-_!@#$%^&*()"
+var getUserChoice = []
+var passwordLength = 0
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-function generatePassword(){
-  passwordLength=parseInt(prompt("Enter Password Length ;)"))
-console.log(passwordLength)
-if (passwordLength >= 8 && passwordLength < 126){
-  return takeUserChoice()
+function generatePassword() {
+  passwordLength = parseInt(prompt("Enter Password Length ;)"))
+  console.log(passwordLength)
+  if (passwordLength >= 8 && passwordLength < 126) {
+    return takeUserChoice()
+  }
+  else {
+    alert("Password has to be between 8 and 125 characters long.")
+    generatePassword()
+  }
 }
-}
-function takeUserChoice(){
-  var yesUpper=confirm("Would you like to have upper case letters in your PW?")
-  var yesLower=confirm("Would you like to have lower case letters in your PW?")
-  var yesNumbers=confirm("Would you like to have upper case letters in your PW?")
-  var yesSymbols=confirm("Would you like to have upper case letters in your PW?")
-  if(yesUpper){
-    getUserChoice+=upperArray
+
+function takeUserChoice() {
+  var yesUpper = confirm("Would you like to have upper case letters in your PW?")
+  var yesLower = confirm("Would you like to have lower case letters in your PW?")
+  var yesNumbers = confirm("Would you like to have numbers in your PW?")
+  var yesSymbols = confirm("Would you like to have symbols in your PW?")
+  getUserChoice = []
+  if (yesUpper) {
+    getUserChoice += upperArray
   }
-  if(yesLower){
-    getUserChoice+=lowerArray
+  if (yesLower) {
+    getUserChoice += lowerArray
   }
-  if(yesNumbers){
-    getUserChoice+=numberArray
+  if (yesNumbers) {
+    getUserChoice += numberArray
   }
-  if(yesSymbols){
-    getUserChoice+=symbolArray
+  if (yesSymbols) {
+    getUserChoice += symbolArray
   }
-  if(getUserChoice.length>0){
+  if (getUserChoice.length > 0) {
     console.log(getUserChoice)
-    var password=""
-    for(let i=0;i<passwordLength;i++){
-     var index=Math.floor ( Math.random()*getUserChoice.length)
-     password += getUserChoice[index]
+    var password = ""
+    for (let i = 0; i < passwordLength; i++) {
+      var index = Math.floor(Math.random() * getUserChoice.length)
+      password += getUserChoice[index]
     }
-    console.log (password)
+    console.log(password)
     return password
   }
+  else {
+    alert("You have to choose at least ONE option...")
+    takeUserChoice()
+  }
 }
 
-
-// Add event listener to generate button
-
-// click event. AKA execute the function.
 generateBtn.addEventListener("click", writePassword);
-
-
-// We need the user to click on the button
-
-// lengty
-//  the inputs we need from the user are. 
-// lower case 
-// upper 
-// symbols 
-// or numbers?
