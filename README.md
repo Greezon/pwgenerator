652cd3bc050e0ecebbe29ee7dccd1e332162e796
Ian A <iastesana@gmail.com>
2020-07-18 18:51:52 -0700

    Finished README.md, added function to stop a bug if user types in a non intiger for length.

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # pwgenerator
 Building a PW generator in Javascript
+
+## Someone suggested building a Password Generator, because it was a good tool to have.
+
+### This project was a true test of attrition for me. I had to use every skill I have obtained to complete it; additionally, I had to look up and learn a mountain of information. The hardest part was learning how to identify bugs, and ultimately fix them.
+
+#### The project was delivered with a variable, a function and an execution. I was not sure what any of these were so I had to look up an incredible amount of information to complete the project.
+
+#### The following items are listed as they appear in my script.js.
+
+	* variables (var)
+		A box that "holds data."
+	* let
+		Like var, but more restrictive.
+	* arrays
+		Stores multiple values in a single variable.
+		Or a collection of Variables of the same type
+	* method
+		Is a property of an object. Or object reference to a property. 
+	* function
+		Is a set of data that performs a task.
+	* DOM
+		Hiarchy of a webpage in this case used to access an element on the website.
+	* if
+		Condition is true do this
+	* else if
+		Condition is false, do this.
+	* prompt
+		Diplays a dialogue, which allows the user to interact.
+	* parseInt
+		Parses a string and returns an integer
+	* console.log
+		Print a data to be displayed to the user.
+	* return
+		Returns the value of the data.
+	* length
+		Returns the length of a string
+	* for loop
+		Repeats a condition till evaluates to false.
+	* addEventListenter
+		Attaches to the specified element without overwriting
+
+This project allowed me to understand how Javascript is used to prompt the user, and get information from the user to control the outcome of their choices. So far this project was by far the most challenging because I am so new to object-oriented programming; however, I learned a lot from it, and will surely use the skills, and most of all the logic on future projects.
+
+https://greezon.github.io/pwgenerator/index.html
+
+![Alt text](./1.png "password-generator")
+
+## Contributing
+Pull requests are welcome. I will not be reviewing any changes from any unknown source.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Assignment Code
-// ID of the button
+// Variables
 var generateBtn = document.querySelector("#generate");
 var upperArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var lowerArray = "abcdefghijklmnopqrstuvwxyz"
@@ -7,8 +6,8 @@ var numberArray = "123456789"
 var symbolArray = "-_!@#$%^&*()"
 var getUserChoice = []
 var passwordLength = 0
-// Write password to the #password input
 
+// Writes the password to the generate password box using querySelector.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -16,29 +15,38 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// starts the specification of th password.
 function generatePassword() {
-  let userStop = prompt("Enter Password Length ;)")
-  if (userStop === null) {
-    alert("Thank you come again, when you need another password.")
+  let userStop = prompt("Enter Password Length.")
+  if (userStop == null) {
+// if user chooses nothing, stops the loop.
+    alert("Thank you come back when you need another password.")
     return "Your Secure Password"
   }
+// if user types non integer tells them invalid input.
+  else if (isNaN(Number(userStop))) {
+    alert("Invalid input, try again...")
+    return generatePassword()
+  }
 
+// if the user types in a number between 8 and 128 it will take "take user choice" or tell the user to use a number between 8 and 128
   passwordLength = parseInt(userStop)
   console.log(passwordLength)
-  if (passwordLength >= 8 && passwordLength < 126) {
+  if (passwordLength >= 8 && passwordLength < 128) {
     return takeUserChoice()
   }
   else {
-    alert("Password has to be between 8 and 126 characters long.")
+    alert("Password has to be between 8 and 128 characters long.")
     return generatePassword()
   }
 }
 
+// Asks the user if they want the various parameters.
 function takeUserChoice() {
-  var yesUpper = confirm("Would you like to have upper case letters in your PW?")
-  var yesLower = confirm("Would you like to have lower case letters in your PW?")
-  var yesNumbers = confirm("Would you like to have #s in your PW?")
-  var yesSymbols = confirm("Would you like to have 5ym80L5 in your PW?")
+  var yesUpper = confirm("Would you like to have upper case letters in your password?")
+  var yesLower = confirm("Would you like to have lower case letters in your password?")
+  var yesNumbers = confirm("Would you like to have numbers in your password?")
+  var yesSymbols = confirm("Would you like to have symbols in your password?")
   getUserChoice = []
   if (yesUpper) {
     getUserChoice += upperArray
@@ -62,10 +70,13 @@ function takeUserChoice() {
     console.log(password)
     return password
   }
+
+// notifies the user to choose 1 option.
   else {
-    alert("You have to choose at least ONE option...")
+    alert("You have to choose at least one option...")
     return takeUserChoice()
   }
 }
 
+// when button is clicked prompts the user to start the parameters of the password
 generateBtn.addEventListener("click", writePassword);
